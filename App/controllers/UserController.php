a3ea76710dd9e5d58374b5af5b370ce9140a08c5
ahmadothman1981 <ahmadothman@outlook.com>
2025-02-17 12:46:32 +0200

    finish authenticate login -register - logout

--- App/controllers/UserController.php
+++ App/controllers/UserController.php
@@ -98,6 +98,7 @@ class UserController
         $this->db->query("INSERT INTO users (name , email , password , city , state) VALUES (:name , :email , :password , :city , :state)", $params);
         // get the new user id
         $userId = $this->db->conn->lastInsertId();
+        //set user session
         Session::set('user' , [
             'id' => $userId,
             'email' => $email,
@@ -105,7 +106,80 @@ class UserController
             'city' => $city,
             'state' => $state
         ]);
-        inspectAndDie(Session::get('user'));
+       
         redirect('/auth/login');
+    }
+    /**
+     * Summary of logout
+     * @return void 
+     */
+    public function logout()
+    {
+        Session::clearAll();
+        //delete cookie
+        $params = session_get_cookie_params();
+        setcookie('PHPSESSID', '', time() - 3600, $params["path"], $params["domain"], $params["secure"], $params["httponly"]);
+        redirect('/');
+    }
+
+    /**
+     * Summary of authenticate
+     * @param array $params
+     * @return void 
+     */
+    public function authenticate()
+    {
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+
+        $errors = [];
+        //validation 
+        if(!Validation::email($email))
+        {
+            $errors['email'] = "Please Enter Valid Email";    
+        }
+        if(!Validation::string($password , 6 , 55))
+        {
+            $errors['password'] = 'Please Enter at least 6 charchters';
+        }
+        
+        if(!empty($errors))
+        {
+            loadView('users/login', [
+                'errors' => $errors
+            ]);
+            exit;
+        }
+        //check for email 
+        $params = [
+            'email' => $email
+        ];
+        $user = $this->db->query('SELECT * FROM users WHERE email = :email', $params)->fetch();
+        if(!$user)
+        {
+            $errors['email'] = "incorrect verified credentials";
+            loadView('users/login',[
+                'errors'=>$errors
+            ]);
+            exit;
+        }
+        //check for password is correct
+        if(!password_verify($password , $user->password))
+        {
+            $errors['password'] = "incorrect verified credentials";
+            loadView('users/login',[
+                'errors'=>$errors
+            ]);
+            exit;
+        }
+        Session::set('user' , [
+            'id' => $user->id,
+            'email' => $user->email,
+            'name' => $user->name,
+            'city' => $user->city,
+            'state' => $user->state
+        ]);
+       
+        redirect('/');
     }
 }
